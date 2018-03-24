1270ce1cf6099e6721b91771103aa95f4d7db6d2
trvski <aetworowsky@gmail.com>
2018-03-24 17:44:18 +1100

    Created post page template

--- index.js
+++ index.js
@@ -9,31 +9,37 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-  var posts = [
-    {
-      position: 1,
-      title: "Blockchain bill passes: Smart Contracts Now Recognized Under Tennessee Law",
+  var posts = [];
+
+  for (var i = 0; i < 25; i++) {
+    posts.push({
+      rank: i + 1,
+      title: "The New Law That Killed Craigslist’s Personals Could End the Web as We’ve Known",
       domain: "coindesk.com",
       score: 121,
       poster: "miyayes",
       age: "4 hours ago",
       comments: 5
-    },
-    {
-      position: 2,
-      title: "America’s first blockchain property-title transfer has been recorded using the Ethereum blockchain",
-      domain: "national.wfgnationaltitle.com",
-      score: 129,
-      poster: "GabeNewell_",
-      age: "9 hours ago",
-      comments: 5
-    },
-  ]
+    });
+  }
 
   response.render('index', { posts: posts })
-})
+});
 
-app.get('/')
+app.get('/post', (request, response) => {
+  var post = {
+    title: "The New Law That Killed Craigslist’s Personals Could End the Web as We’ve Known",
+    domain: "coindesk.com",
+    score: 121,
+    poster: "miyayes",
+    age: "4 hours ago",
+    comments: 5
+  }
+
+  response.render('post', { post: post })
+});
+
+app.get('/');
 
 exports.http = app;
 
