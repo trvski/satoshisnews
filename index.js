62e3e07f9e4b30d3e5b97c661df4dbd67167a86d
trvski <aetworowsky@gmail.com>
2018-03-26 15:05:03 +1100

    Added Hacker News API for test data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require("express");
+const posts = require("./models/posts");
 
 const app = express();
 
@@ -8,86 +9,20 @@ app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'));
 
-var data = {
-  posts: []
-}
-
-for (var i = 0; i < 25; i++) {
-  data.posts.push({
-    rank: i + 1,
-    title: "The New Law That Killed Craigslist’s Personals Could End the Web as We’ve Known",
-    domain: "coindesk.com",
-    score: 121,
-    poster: "miyayes",
-    age: "4 hours ago",
-    comments: [
-      {
-        body: "The real story is he has no employment rights because he's in America and he's",
-        score: 121,
-        author: "miyayes",
-        age: "4 hours ago",
-        replies: [
-          {
-            body: "The real story is he has no employment rights because he's in America and he's",
-            score: 121,
-            author: "miyayes",
-            age: "4 hours ago",
-            replies: []
-          },
-          {
-            body: "The real story is he has no employment rights because he's in America and he's",
-            score: 121,
-            author: "miyayes",
-            age: "4 hours ago",
-            replies: []
-          },
-          {
-            body: "The real story is he has no employment rights because he's in America and he's",
-            score: 121,
-            author: "miyayes",
-            age: "4 hours ago",
-            replies: []
-          },
-          {
-            body: "The real story is he has no employment rights because he's in America and he's",
-            score: 121,
-            author: "miyayes",
-            age: "4 hours ago",
-            replies: []
-          }
-        ]
-      },
-      {
-        body: "The real story is he has no employment rights because he's in America and he's",
-        score: 121,
-        author: "miyayes",
-        age: "4 hours ago",
-        replies: []
-      },
-      {
-        body: "The real story is he has no employment rights because he's in America and he's",
-        score: 121,
-        author: "miyayes",
-        age: "4 hours ago",
-        replies: []
-      },
-      {
-        body: "The real story is he has no employment rights because he's in America and he's",
-        score: 121,
-        author: "miyayes",
-        age: "4 hours ago",
-        replies: []
-      }
-    ]
-  });
-}
-
 app.get('/', (request, response) => {
-  response.render('index', data)
+  posts
+    .getTopPosts()
+    .then(posts => {
+      response.render('index', {posts: posts});
+    });
 });
 
-app.get('/post', (request, response) => {
-  response.render('post', {post: data.posts[0]})
+app.get('/post/:id', (request, response) => {
+  posts
+    .getPost(request.param('id'))
+    .then(post => {
+      response.render('post', {post: post});
+    });
 });
 
 app.get('/');
